d71122d17faee6a7f80c3b831b605628af04cdba
Devrandom <miron@basezero.com>
2019-08-22 21:29:55 +0200

    cleanup

--- mw/mw.py
+++ mw/mw.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python
-import sha3
-from optparse import OptionParser
-from mnemonic.mnemonic import Mnemonic
-import sys
-import os
 import hashlib
-from binascii import hexlify, unhexlify
-from pycoin.key.BIP32Node import BIP32Node
-from pycoin.networks import full_network_name_for_netcode, network_name_for_netcode
-from pycoin.encoding import b2a_hashed_base58, to_bytes_32
+import os
+import sys
+from binascii import hexlify
 from getpass import getpass
+from optparse import OptionParser
+
+import hashprint
+import sha3
+from mnemonic.mnemonic import Mnemonic
+from past.builtins import raw_input
+from pycoin.encoding import b2a_hashed_base58, to_bytes_32
+from pycoin.key.BIP32Node import BIP32Node
+
 from .colorize import colorize
 from .ripple import RippleBaseDecoder
-import hashprint
 
 # mw 'abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about' TREZOR
 # > seed c55257c360c07c72029aebc1b53c05ed0362ada38ead3e3e9efa3708e53495531f09a6987599d18264c1e1c92f2cf141630c7a3c4ab7c81b2f001698e7463b04
@@ -24,42 +26,52 @@ VISUALIZATION_PATH = "9999'/9999'"
 
 ripple_decoder = RippleBaseDecoder()
 
+
 def btc_to_address(prefix, subkey):
     return b2a_hashed_base58(prefix + subkey.hash160())
 
+
 def btc_to_private(exponent):
     return b2a_hashed_base58(b'\x80' + to_bytes_32(exponent) + b'\01')
+
 
 def eth_to_address(prefix, subkey):
     hasher = sha3.keccak_256()
     hasher.update(subkey.sec(True)[1:])
     return hexlify(hasher.digest()[-20:]).decode()
 
+
 def eth_to_private(exponent):
     return hexlify(to_bytes_32(exponent)).decode()
+
 
 def xrp_to_address(prefix, subkey):
     return ripple_decoder.encode(subkey.hash160())
 
+
 def xrp_to_private(exponent):
     return hexlify(to_bytes_32(exponent)).decode()
 
+
 coin_map = {
-        "btc": (b'\0', "44'/0'/0'/0", btc_to_address, btc_to_private),
-        "zcash": (b'\x1c\xb8', "44'/1893'/0'/0", btc_to_address, btc_to_private),
-        "eth": (b'', "44'/60'/0'/0", eth_to_address, eth_to_private),
-        "rop": (b'', "44'/1'/0'/0", eth_to_address, eth_to_private),
-        "xrp": (b'', "44'/144'/0'/0", xrp_to_address, xrp_to_private),
-        "txrp": (b'', "44'/1'/0'/0", xrp_to_address, xrp_to_private),
-        }
+    "btc": (b'\0', "44'/0'/0'/0", btc_to_address, btc_to_private),
+    "zcash": (b'\x1c\xb8', "44'/1893'/0'/0", btc_to_address, btc_to_private),
+    "eth": (b'', "44'/60'/0'/0", eth_to_address, eth_to_private),
+    "rop": (b'', "44'/1'/0'/0", eth_to_address, eth_to_private),
+    "xrp": (b'', "44'/144'/0'/0", xrp_to_address, xrp_to_private),
+    "txrp": (b'', "44'/1'/0'/0", xrp_to_address, xrp_to_private),
+}
+
 coins = list(coin_map.keys())
 
 coin_list = ",".join(coins)
 
+
 def mnemonic_to_master(mnemonic, passphrase):
     seed = Mnemonic.to_seed(mnemonic, passphrase=passphrase)
     master = BIP32Node.from_master_secret(seed)
-    return (seed, master)
+    return seed, master
+
 
 def compute_address(coin, master, i):
     (address_prefix, coin_derivation, to_address, to_private) = coin_map[coin]
@@ -67,20 +79,24 @@ def compute_address(coin, master, i):
     subkey = next(master.subkeys(path))
     private = to_private(subkey.secret_exponent())
     address = to_address(address_prefix, subkey)
-    return (address, private)
+    return address, private
+
 
 def generate(data=None):
     if data is None:
         data = os.urandom(16)
     return Mnemonic('english').to_mnemonic(data)
 
+
 def hash_entropy(entropy_string):
     ee = hashlib.sha256(entropy_string.encode('utf-8'))
     return ee.digest()[0:16]
 
+
 def visual(master):
     subkey = next(master.subkeys(VISUALIZATION_PATH))
     return hashprint.pformat(list(bytearray(subkey.hash160())))
+
 
 def main():
     parser = OptionParser()
@@ -100,14 +116,14 @@ def main():
         sys.exit(1)
 
     entropy = None
-    if (options.entropy):
+    if options.entropy:
         sys.stdout.write("Enter entropy string followed by a \\n. ")
         sys.stdout.write("No entropy is added, make sure you provide enough.\n")
         sys.stdout.write(": ")
         entropy_string = raw_input()
         entropy = hash_entropy(entropy_string)
     
-    if (options.generate):
+    if options.generate:
         print(generate(entropy))
         exit()
 
@@ -128,9 +144,10 @@ def main():
     for i in range(options.count):
         (address, private) = compute_address(options.coin, master, i)
         if options.private:
-            print("%s %s"%(address, private))
+            print("%s %s" % (address, private))
         else:
-            print("%s"%(address))
+            print("%s" % (address,))
+
 
 if __name__ == "__main__":
     main()
--- tests/mw_test.py
+++ tests/mw_test.py
@@ -1,15 +1,19 @@
 import unittest
-from mw.mw import mnemonic_to_master, compute_address, generate, hash_entropy, visual
-from binascii import hexlify, unhexlify
+from binascii import hexlify
+
+from mw.mw import compute_address, generate, hash_entropy, mnemonic_to_master, visual
 
 mnemonic1 = 'license diagram pelican spy monitor convince damage script wall hockey goose month popular swamp sugar rose mystery gap regular acquire bottom sea modify eyebrow'
 mnemonic2 = 'abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon abandon about'
+
+
 class TestAll(unittest.TestCase):
     def test_ripple(self):
         (seed, master) = mnemonic_to_master(mnemonic1, '')
         (a0, p0) = compute_address('xrp', master, 0)
         self.assertEqual(a0, 'r4keqeEf57QZnLeiKr5f4twBoH5Mr9S824')
         self.assertEqual(p0, 'f730f2c7a79fabe94465b2d86f6c7f108d56810d89965f99c6bf193cfc7b0730')
+
     def test_eth(self):
         (seed, master) = mnemonic_to_master(mnemonic1, '')
         (a0, p0) = compute_address('eth', master, 0)
@@ -18,9 +22,11 @@ class TestAll(unittest.TestCase):
         (a10, p10) = compute_address('eth', master, 10)
         self.assertEqual(a10, 'a6c572a1e51b377fd610861da4b64e6df987bea6')
         self.assertEqual(p10, 'de77e2f9b063355d6e1b98af2717a21020cca03bb3749d86155ded10f2abd775')
+
     def test_seed(self):
         (seed, master) = mnemonic_to_master(mnemonic2, 'TREZOR')
         self.assertEquals(visual(master), '+-----------------+\n|                 |\n|                 |\n|                 |\n|                 |\n|       .S       =|\n|*.    o=*=     +B|\n|@*     o.*+     .|\n|.      .*+.      |\n|o ...        .E  |\n+-----------------+')
+
     def test_btc(self):
         (seed, master) = mnemonic_to_master(mnemonic2, 'TREZOR')
         self.assertEquals(hexlify(seed), b'c55257c360c07c72029aebc1b53c05ed0362ada38ead3e3e9efa3708e53495531f09a6987599d18264c1e1c92f2cf141630c7a3c4ab7c81b2f001698e7463b04')
@@ -30,6 +36,7 @@ class TestAll(unittest.TestCase):
         (a10, p10) = compute_address('btc', master, 10)
         self.assertEqual(a10, '1LfvmsJpjSc5xh3QpM9cdxxrHSmD63UyVt')
         self.assertEqual(p10, 'Kwq9q38wfkuVZXVDcAruUUvH9xNERaxvMUkrTRu8uJVp25xEnAyg')
+
     def test_zcash(self):
         (seed, master) = mnemonic_to_master(mnemonic2, 'TREZOR')
         (a0, p0) = compute_address('zcash', master, 0)
@@ -38,11 +45,13 @@ class TestAll(unittest.TestCase):
         (a10, p10) = compute_address('zcash', master, 10)
         self.assertEqual(a10, 't1KgukGc7CdKgG87c6yD5E4UkB3rDEv3BXt')
         self.assertEqual(p10, 'KxyEPn1nUukhZFhBb69MSUYay4ojAe7SFGcW6doyqvEEuERQES2G')
+
     def test_generate(self):
         self.assertEqual(12, len(generate().split(" ")))
 
     def test_entropy(self):
-        self.assertEqual(u"coconut mystery hub satoshi any mandate option alter client column judge diamond", generate(hash_entropy("hello")))
+        self.assertEqual(u"coconut mystery hub satoshi any mandate option alter client column judge diamond",
+                         generate(hash_entropy("hello")))
         
 if __name__ == '__main__':
     unittest.main()
